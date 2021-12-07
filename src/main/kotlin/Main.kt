75a4785e7d5fd906a078b94a75f2fa993a28540c
Christoph Kainz <ckainz11@users.noreply.github.com>
2021-12-07 10:43:02 +0100

    solve day 7 as single expression

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
-import day7.solve1
-import day7.solve2
+import day7.solve1OL
+import day7.solve2OL
 import kotlin.system.measureTimeMillis
 
 fun main() {
     println("Starting AdventOfCode 2021 - Merry Christmas!")
     val time1 = measureTimeMillis {
-        println("Solution for part 1: ${solve1()}")
+        println("Solution for part 1: ${solve1OL()}")
     }
     println("In ${time1} ms")
     val time2 = measureTimeMillis {
-        println("Solution for part 2: ${solve2()}")
+        println("Solution for part 2: ${solve2OL()}")
     }
     println("In ${time2} ms")
 }
--- src/main/kotlin/day7/Day7.kt
+++ src/main/kotlin/day7/Day7.kt
@@ -25,3 +25,18 @@ fun solve2(): Int {
 fun calcCost(align: Int): Int = input.sumOf { abs(it - align) }
 fun calcCrabCost(align: Int) = input.sumOf { calcTriangularNumber(abs(it-align)) }
 private fun calcTriangularNumber(num:Int):Int = (1..num).sumOf { it }
+
+fun solve1OL(): Int = getInputAsText(7).split(",").map(String::toInt).run {
+    (this.minOrNull()!!..this.maxOrNull()!!).minOf { dst ->
+        this.sumOf { crab ->
+            abs(crab - dst)
+        }
+    }
+}
+fun solve2OL(): Int = getInputAsText(7).split(",").map(String::toInt).run {
+    (this.minOrNull()!!..this.maxOrNull()!!).minOf { dst ->
+        this.sumOf { crab ->
+            (0).rangeTo(abs(crab - dst)).sumOf { it }
+        }
+    }
+}
